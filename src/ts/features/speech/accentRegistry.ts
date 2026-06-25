3e24a0055218e11eac1952b6cb81fd1845420a33
Skarpelen <86878931+Skarpelen@users.noreply.github.com>
2026-06-25 12:48:31 +0500

    feat: больше акцентов

--- src/ts/features/speech/accentRegistry.ts
+++ src/ts/features/speech/accentRegistry.ts
@@ -2,10 +2,31 @@ import {
   createReplacementAccentData,
   type ReplacementAccentData
 } from "./replacementAccent";
-import dwarfAccentFtl from "./data/dwarf.ftl?raw";
-import wordReplacementsYaml from "./data/word_replacements.yml?raw";
 
-const ACCENTS = createReplacementAccentData(wordReplacementsYaml, dwarfAccentFtl);
+import demonicFtl from "./data/accents/demonic.ftl?raw";
+import demonicYaml from "./data/accents/demonic.yml?raw";
+import dwarfFtl from "./data/accents/dwarf.ftl?raw";
+import dwarfYaml from "./data/accents/dwarf.yml?raw";
+import oldcommonFtl from "./data/accents/oldcommon.ftl?raw";
+import oldcommonYaml from "./data/accents/oldcommon.yml?raw";
+import pirateFtl from "./data/accents/pirate.ftl?raw";
+import pirateYaml from "./data/accents/pirate.yml?raw";
+
+const ACCENT_YAML = [
+  dwarfYaml,
+  oldcommonYaml,
+  pirateYaml,
+  demonicYaml
+].join("\n");
+
+const ACCENT_FTL = [
+  dwarfFtl,
+  oldcommonFtl,
+  pirateFtl,
+  demonicFtl
+].join("\n");
+
+const ACCENTS = createReplacementAccentData(ACCENT_YAML, ACCENT_FTL);
 
 export function getAccentData(accentId: string | undefined): ReplacementAccentData | undefined {
   return accentId ? ACCENTS.get(accentId) : undefined;
